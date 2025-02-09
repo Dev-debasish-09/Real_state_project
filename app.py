7444ce9751ec50911ee1474a8601eed0dcb6fe52
DEBASISH PRADHAN <161795696+Dev-debasish-09@users.noreply.github.com>
2025-02-09 19:07:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import seaborn as sns
+st.set_page_config(page_title='My prediction and analysis')
+
+st.title("_Welcome To My Home Page_")
+st.image("D:\cap-proj-01\Gurgaon image.jpg")
+
